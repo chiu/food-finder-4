53a5117884ed80feca19a75d6890d14bde40b9e4
chiu <chiucode@gmail.com>
2015-09-26 20:09:04 +0000

    bugs all fixed

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -19,7 +19,8 @@ class DealsController < ApplicationController
         @deal = @diner.deals.build(deal_params) # @deal.user_id = current_user.id
 
         if @deal.save
-            redirect_to @diner, notice: "Review created successfully"
+            redirect_to diners_path, notice: "Review created successfully"
+            # redirect_to @diner, notice: "Review created successfully"
         else
             render :new
         end
