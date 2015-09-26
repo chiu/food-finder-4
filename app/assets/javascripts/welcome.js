b59267ba3298ef40d9fbacc0589c64acf3fe37d3
jsivakumaran <jsivakumaran@outlook.com>
2015-09-26 01:41:55 -0700

    front-end navbar

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,16 +1,61 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+/* Create an array to hold the different image positions */
+var itemPositions = [];
+var numberOfItems = $('#scroller .item').length;
 
-$('section.awSlider .carousel').carousel({
-	pause: "hover",
-  interval: 2000
-});
+/* Assign each array element a CSS class based on its initial position */
+function assignPositions() {
+    for (var i = 0; i < numberOfItems; i++) {
+        if (i === 0) {
+            itemPositions[i] = 'left-hidden';
+        } else if (i === 1) {
+            itemPositions[i] = 'left';
+        } else if (i === 2) {
+            itemPositions[i] = 'middle';
+        } else if (i === 3) {
+            itemPositions[i] = 'right';
+        } else {
+            itemPositions[i] = 'right-hidden';
+        }
+    }
+    /* Add each class to the corresponding element */
+    $('#scroller .item').each(function(index) {
+        $(this).addClass(itemPositions[index]);
+    });
+}
 
-var startImage = $('section.awSlider .item.active > img').attr('src');
-$('section.awSlider').append('<img src="' + startImage + '">');
+/* To scroll, we shift the array values by one place and reapply the classes to the images */
+function scroll(direction) {
+    if (direction === 'prev') {
+        itemPositions.push(itemPositions.shift());
+    } else if (direction === 'next') {
+        itemPositions.unshift(itemPositions.pop());
+    }
+    $('#scroller .item').removeClass('left-hidden left middle right right-hidden').each(function(index) {
+        $(this).addClass(itemPositions[index]);
+    });
+}
 
-$('section.awSlider .carousel').on('slid.bs.carousel', function () {
- var bscn = $(this).find('.item.active > img').attr('src');
-	$('section.awSlider > img').attr('src',bscn);
+/* Do all this when the DOMs ready */
+$(document).ready(function() {
+
+    assignPositions();
+    var autoScroll = window.setInterval("scroll('next')", 4000);
+
+    /* Hover behaviours */
+    $('#scroller').hover(function() {
+        window.clearInterval(autoScroll);
+        $('.nav').stop(true, true).fadeIn(200);
+    }, function() {
+        autoScroll = window.setInterval("scroll('next')", 4000);
+        $('.nav').stop(true, true).fadeOut(200);
+    });
+
+    /* Click behaviours */
+    $('.prev').click(function() {
+        scroll('prev');
+    });
+    $('.next').click(function() {
+        scroll('next');
+    });
+
 });
