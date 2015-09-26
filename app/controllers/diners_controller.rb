367aabd3223d581f3a4e184fbbfd080efde4c0ef
chiu <chiucode@gmail.com>
2015-09-26 05:35:40 +0000

    added diner signup form

--- app/controllers/diners_controller.rb
+++ app/controllers/diners_controller.rb
@@ -5,3 +5,28 @@ class DinersController < ApplicationController
   def create
   end
 end
+
+
+ class DinersController < ApplicationController
+
+      def new
+        @diner = Diner.new
+      end
+
+      def create
+        @diner = Diner.new(diner_params)
+
+        if @diner.save
+          # redirect_to movies_path
+        else
+          render :new
+        end
+      end
+
+      protected
+
+      def diner_params
+        params.require(:diner).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+      end
+
+    end
