befa86f19276dd51b29744817ae60c1d35d210b7
chiu <chiucode@gmail.com>
2015-09-26 08:33:56 +0000

    fixed controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,27 @@
-class DealsController < ApplicationController
-  def new
-  end
 
-  def create
-  end
+
+class DealsController < ApplicationController
+
+    def new
+        @diner = Diner.find(params[:diner_id])
+        @deal = @diner.deals.build
+    end
+
+    def create
+        @diner = Diner.find(params[:diner_id])
+        @deal = @diner.deals.build(deal_params) # @deal.user_id = current_user.id
+
+        if @deal.save
+            redirect_to @diner, notice: "Review created successfully"
+        else
+            render :new
+        end
+    end
+
+    protected
+
+    def deal_params
+        params.require(:deal).permit(:name)
+    end
+
 end
